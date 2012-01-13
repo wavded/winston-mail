d338bcc22467862e9bcfeefffe4019dc4c5a5718
Marc Harter <wavded@gmail.com>
2012-01-13 12:07:02 -0600

    Added dummy SMTP server

--- test/winston-mail-test.js
+++ test/winston-mail-test.js
@@ -8,9 +8,11 @@ var vows = require('vows');
 var assert = require('assert');
 var winston = require('winston');
 var helpers = require('winston/test/helpers');
+var spawn = require('child_process').spawn;
 var Mail = require('../lib/winston-mail').Mail;
 
-console.log('Note this test file uses a internal SMTP server, modify fixtures/test-config to your SMTP server');
+var smtp = spawn('java',['-jar','test/fixtures/DevNullSmtp.jar','-p','2500','-console'])
+smtp.stderr.on('data', function (data) { console.log('stderr: ' + data); });
 
 function assertMail (transport) {
   assert.instanceOf(transport, Mail);
@@ -21,6 +23,10 @@ var config = helpers.loadConfig(__dirname)
 var transport = new (Mail)(config.transports.mail);
 
 vows.describe('winston-mail').addBatch({
+ "Start SMTP Server": {
+    topic: function () { setTimeout(this.callback, 1000) }, '': function () { assert.ok(true) }
+ }
+}).addBatch({
  "An instance of the Mail Transport": {
    "should have the proper methods defined": function () {
      assertMail(transport);
@@ -29,5 +35,9 @@ vows.describe('winston-mail').addBatch({
      assert.isTrue(!err);
      assert.isTrue(logged);
    })
+ }
+}).addBatch({
+ "Stop SMTP Server": {
+    topic: function (){ smtp.kill(); setTimeout(this.callback, 500) }, '': function () { assert.ok(true) }
  }
 }).export(module);
