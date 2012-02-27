716f927be97ebd0ebb000a1aaa66fac179b31d80
Marc Harter <wavded@gmail.com>
2012-02-27 16:13:08 -0600

    Updated test suite

--- test/winston-mail-test.js
+++ test/winston-mail-test.js
@@ -8,12 +8,12 @@ var vows = require('vows');
 var assert = require('assert');
 var winston = require('winston');
 var helpers = require('winston/test/helpers');
-var spawn = require('child_process').spawn;
+var smtp = require('simplesmtp').createServer();
 var Mail = require('../lib/winston-mail').Mail;
 
-var smtp = spawn('java',['-jar','test/fixtures/DevNullSmtp.jar','-p','2500','-console'])
-smtp.stdout.on('data', function (data) { console.log('stdout: ' + data); });
-smtp.stderr.on('data', function (data) { console.log('stderr: ' + data); });
+smtp.listen(2500, function (err) {
+   console.log(arguments)
+})
 
 function assertMail (transport) {
   assert.instanceOf(transport, Mail);
@@ -24,21 +24,13 @@ var config = helpers.loadConfig(__dirname)
 var transport = new (Mail)(config.transports.mail);
 
 vows.describe('winston-mail').addBatch({
- "Start SMTP Server": {
-    topic: function () { setTimeout(this.callback, 1000) }, '': function () { assert.ok(true) }
- }
-}).addBatch({
  "An instance of the Mail Transport": {
    "should have the proper methods defined": function () {
      assertMail(transport);
    },
-   // "the log() method": helpers.testNpmLevels(transport, "should log messages to Mail", function (ign, err, logged) {
-   //   assert.isTrue(!err);
-   //   assert.isTrue(logged);
-   // })
- }
-}).addBatch({
- "Stop SMTP Server": {
-    topic: function (){ smtp.kill(); setTimeout(this.callback, 500) }, '': function () { assert.ok(true) }
+   "the log() method": helpers.testNpmLevels(transport, "should log messages to Mail", function (ign, err, logged) {
+     assert.isTrue(!err);
+     assert.isTrue(logged);
+   })
  }
 }).export(module);
